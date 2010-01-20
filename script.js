3c8cfcad70d936b4b6d61190f0fbb01525017366
nathanielksmith <nathanielksmith@gmail.com>
2010-01-19 22:00:57 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// list: http://twitter.com/statuses/friends.format
+
+var status = $("#status");
+
+// XXX get list of friends
+    // XXX populate autocomplete
+
+// XXX add keypress event listener to status
+    // XXX trigger autocomplete on '@'
+
