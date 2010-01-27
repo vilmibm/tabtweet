06b01102c288aa7dcc45a22951d69c441b8e3567
nate <nate@kafka.(none)>
2010-01-26 22:55:22 -0500

    Step one of OAuth complete after a frustrating hour of mortal kombat.

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ var friends = "http://twitter.com/statuses/friends.json";
 var statusbox = $("#status");
 
 var data;
-
-$.getJSON(friends, function(json) {
-    data = $.map(json, function(n,i) {
-       return "@" + n.screen_name; 
+if (statusbox) {
+    $.getJSON(friends, function(json) {
+        data = $.map(json, function(n,i) {
+           return "@" + n.screen_name;
+        });
+        statusbox.autocomplete(
+            data.sort(
+                function(a,b){return a.toLowerCase()>b.toLowerCase()?1:-1; }
+            )
+        );
     });
-    statusbox.autocomplete(
-        data.sort(
-            function(a,b){return a.toLowerCase()>b.toLowerCase()?1:-1; }
-        )
-    );	
-});
-
+}
 // XXX todo:
     // 1. all friends, not just 100
     // 2. animating "working" plugin icon
