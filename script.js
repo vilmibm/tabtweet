9e8957483a8ca4452f3c3aeb1dc856b3280d86df
nathanielksmith <nathanielksmith@gmail.com>
2010-01-31 21:35:59 -0500

    code cleanup on oauth process

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 var friends_url = "http://twitter.com/statuses/friends.json";
 var statusbox = $("#status");
-var message = {
-    action : friends_url,
-    method : "GET",
-    parameters : {}
-};
-var accessor = {
-    consumerKey:    'ruXg6gDIBxihEyMd9jCHdQ',
-    consumerSecret: '5SLxFQ0WrXPfOC11dbIq8ASihJvecHEvHK4YvLuQq4',
-    token: '',
-    tokenSecret: '',
-};
+chrome.extension.sendRequest('consumer', function(response) {
+    var consumer_key    = response[0];
+    var consumer_secret = response[1];
 
-chrome.extension.sendRequest('', function(response) {
-    accessor.token       = response[0];
-    accessor.tokenSecret = response[1];
-    OAuth.completeRequest(message, accessor);
-    OAuth.SignatureMethod.sign(message, accessor);
-    friends_url = friends_url + '?' + OAuth.formEncode(message.parameters);
+    var message = {
+        action : friends_url,
+        method : "GET",
+        parameters : {}
+    };
+    var accessor = {
+        consumerKey:    consumer_key,
+        consumerSecret: consumer_secret,
+        token: '',
+        tokenSecret: '',
+    };
 
-    if ( accessor.token ) {
-        var data;
-        $.getJSON(friends_url, function(json) {
-            data = $.map(json, function(n,i) {
-               return "@" + n.screen_name;
+    chrome.extension.sendRequest('access', function(response) {
+        accessor.token       = response[0];
+        accessor.tokenSecret = response[1];
+        OAuth.completeRequest(message, accessor);
+        OAuth.SignatureMethod.sign(message, accessor);
+        friends_url = friends_url + '?' + OAuth.formEncode(message.parameters);
+
+        if ( accessor.token ) {
+            var data;
+            $.getJSON(friends_url, function(json) {
+                data = $.map(json, function(n,i) {
+                   return "@" + n.screen_name;
+                });
+                statusbox.autocomplete(
+                    data.sort(
+                        function(a,b){return a.toLowerCase()>b.toLowerCase()?1:-1; }
+                    )
+                );
             });
-            statusbox.autocomplete(
-                data.sort(
-                    function(a,b){return a.toLowerCase()>b.toLowerCase()?1:-1; }
-                )
-            );
-        });
-    }
+        }
+    });
 });
 
 // XXX todo:
