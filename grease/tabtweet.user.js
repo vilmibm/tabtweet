c11f70bd81dbf99b98a1055b9e6fe0d139ccbbb7
nathanielksmith <nathanielksmith@gmail.com>
2010-02-10 21:59:55 -0500

    doc

---
+++ grease/tabtweet.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name tabtweet
+// @namespace http://chiptheglasses.com
+// @description add friend name autocomplete to twitter
+// @include http://twitter.com
+// @exclude *
+// ==/UserScript==
